0007e07662a92e57d4a042bbdf8b46fbffe76e42
insideoutzombie <david@web3devs.com>
2018-05-11 23:03:44 -0500

    adds a ton of code

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,28 +1,31 @@
+require('events').EventEmitter.defaultMaxListeners = 0;
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
 
+
 beforeEach(async () => {
   // Get list of all accounts
 accounts = await web3.eth.getAccounts();
-
-
 
 // Use one of those account to deploy
 
 // the contract
 inbox = await new web3.eth.Contract(JSON.parse(interface))
    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-   .send({ from: accounts[0], gas: '1000000' })
+   .send({ from: accounts[0], gas: '1000000' });
+    inbox.setProvider(provider);
 });
 
 
  describe('Inbox', () => {
-   it('deploys a contract', () => {});
-   console.log(inbox);
+   it('deploys a contract', () => {
+   assert.ok(inbox.options.address);
  });
+});
